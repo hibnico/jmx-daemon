7801152e20e2028148742d7a4c67e6384b9801fb
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2013-01-13 17:47:49 +0100

    improve inline help

--- src/main/java/org/hibnet/jmxdaemon/JmxDaemon.java
+++ src/main/java/org/hibnet/jmxdaemon/JmxDaemon.java
@@ -70,10 +70,19 @@ public class JmxDaemon {
         int port = 2713;
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("-h") || args[i].equals("--help")) {
-                System.out.println("Usage:");
-                System.out.println("    -p, --port <port>          : the port number to listen to (defaults to 2713)");
-                System.out
-                        .println("    -l, --liten-address <host> : the address the daemon will listen to (defaults to localhost)");
+                if (args.length > i) {
+                    String cmd = args[++i].toUpperCase();
+                    if (cmd.equals(JmxRequestHandler.REQ_CMD_GET)) {
+                        displayHelpGET();
+                    } else if (cmd.equals(JmxRequestHandler.REQ_CMD_CLOSE)) {
+                        displayHelpCLOSE();
+                    } else {
+                        System.err.println("Unknwon command " + cmd);
+                        System.exit(1);
+                    }
+                } else {
+                    displayHelp();
+                }
                 System.exit(0);
             }
             if (args[i].equals("-p") || args[i].equals("--port")) {
@@ -89,11 +98,11 @@ public class JmxDaemon {
                     System.exit(1);
                 }
             }
-            if (args[i].equals("-l") || args[i].equals("--listen-address")) {
+            if (args[i].equals("-l") || args[i].equals("--listen")) {
                 if (args.length > i) {
                     listendAddress = args[++i];
                 } else {
-                    System.err.println("Expecting a listen address after '-l' or '--listen-address' option");
+                    System.err.println("Expecting a listen address after '-l' or '--listen' option");
                     System.exit(1);
                 }
             }
@@ -102,5 +111,40 @@ public class JmxDaemon {
         InternalLoggerFactory.setDefaultFactory(new Slf4JLoggerFactory());
         JmxDaemon daemon = new JmxDaemon(new InetSocketAddress(listendAddress, port));
         daemon.start();
+    }
+
+    private static void displayHelp() {
+        System.out.println("Usage:");
+        System.out.println("    -h, --help          : display this help");
+        System.out.println("    -h, --help <cmd>    : display the help about a specific command");
+        System.out.println("                          supported commands are: GET, CLOSE");
+        System.out.println("    -p, --port <port>   : the port number to listen to (defaults to 2713)");
+        System.out.println("    -l, --listen <host> : the address the daemon will listen to (defaults to localhost)");
+    }
+
+    private static void displayHelpGET() {
+        System.out.println("The GET command retrieve values from JMX beans and returns the values formatted according");
+        System.out.println("  the to specified format");
+        System.out.println("Usage:");
+        System.out.println("  GET <jmxurl> <stringformat> [<beanname> <attribute>]+");
+        System.out.println("    jmxurl       : the url of the JMX endpoint");
+        System.out.println("    stringformat : the string format of how attributes should be outputed back");
+        System.out.println("    beanname     : the name of the JMX bean to query");
+        System.out.println("    attribute    : the name of the attribute to get the value from");
+        System.out.println("Exemple:");
+        System.out.println("  GET service:jmx:rmi:///jndi/rmi://myserver.mydomain.com:7199/jmxrmi \\");
+        System.out.println("    'used:%d committed:%d max:%d' \\");
+        System.out.println("    java.lang:type=Memory HeapMemoryUsage.used \\");
+        System.out.println("    java.lang:type=Memory HeapMemoryUsage.committed \\");
+        System.out.println("    java.lang:type=Memory HeapMemoryUsage.max");
+    }
+
+    private static void displayHelpCLOSE() {
+        System.out.println("The CLOSE command close the closing to JMX endpoint which might have been cached");
+        System.out.println("Usage:");
+        System.out.println("  CLOSE <jmxurl>");
+        System.out.println("    jmxurl : the url of the JMX endpoint");
+        System.out.println("Exemple:");
+        System.out.println("  CLOSE service:jmx:rmi:///jndi/rmi://myserver.mydomain.com:7199/jmxrmi");
     }
 }
