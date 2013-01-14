bc8aaa225e87855125ed83edc917f1c5b262e7e8
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2013-01-14 15:19:25 +0100

    Use the english locale to format float numbers

--- src/main/java/org/hibnet/jmxdaemon/JmxRequestHandler.java
+++ src/main/java/org/hibnet/jmxdaemon/JmxRequestHandler.java
@@ -18,6 +18,7 @@ package org.hibnet.jmxdaemon;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -144,7 +145,7 @@ public class JmxRequestHandler extends SimpleChannelHandler {
                     }
                     if (values != null) {
                         try {
-                            String output = String.format(format, values.toArray());
+                            String output = String.format(Locale.ENGLISH, format, values.toArray());
                             response.append(RESP_OK);
                             response.append(RESP_SEP);
                             response.append(output);
